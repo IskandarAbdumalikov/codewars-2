b7ca1e6f5e5ffaa266c748d5d0b69fbef8bc8eb0
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-03-20 19:35:39 +0500

    92

---
+++ app.js
@@ -0,0 +1,32 @@
+// function monkeyCount(n) {
+//   let count = [];
+//   for (let i = 1; i <= n; i++) {
+//     count.push(i);
+//   }
+//   return count;
+// }
+
+// {
+//   function getNumberFromString(s) {
+//     let result = "";
+//     for (let i = 0; i < s.length; i++) {
+//       if (!isNaN(+s[i])) {  //agar i ninchi o`rindagi string son bo`lsa uni num ga o`zalshtir .uni isNaN bilan tekshirib ishladim
+//         result += s[i];
+//       }
+//     }
+//     let number = +result;
+//     return number;
+//   }
+//   console.log(getNumberFromString("sdr4kj6"));
+// }
+
+// {
+//   function between(a, b) {
+//     let all =[]
+//     for (let i = a; i < b+1; i++) {
+//       all.push(i)
+//     }
+//     return all
+//   }
+//   console.log(1,4);
+// }
